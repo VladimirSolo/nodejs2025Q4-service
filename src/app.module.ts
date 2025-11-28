56d3e7661c55cd676ca2f362751b90142cbd5e4f
Vladimir Solo <freeman.wushu@mail.ru>
2025-11-28 21:00:52 +0300

    feat: create albums

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ArtistsModule } from './artists/artists.module';
 import { TracksModule } from './tracks/tracks.module';
+import { AlbumsModule } from './albums/albums.module';
 
 @Module({
-  imports: [UserModule, ArtistsModule, TracksModule],
+  imports: [UserModule, ArtistsModule, TracksModule, AlbumsModule],
   controllers: [AppController],
   providers: [AppService],
 })
